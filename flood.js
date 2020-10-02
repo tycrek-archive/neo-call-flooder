79d3d081ddd445ff9a0643169d2561dc37bdf147
tycrek <josh.moore@jmoore.dev>
2020-10-02 08:09:57 -0600

    option to send digits on connect

--- flood.js
+++ flood.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const client = new require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Set this to 0 for infinite
-const max = 10;
+const max = 0;
 
 // Don't change this
 let count = 1;
@@ -17,7 +17,7 @@ process.on('SIGINT', finished);
 
 function placeCall() {
 	return new Promise((resolve, reject) =>
-		client.calls.create({ record: true, url: process.env.TWIML_URL, to: process.env.TARGET_PHONE_NUMBER, from: process.env.SOURCE_PHONE_NUMBER })
+		client.calls.create({ record: true, url: process.env.TWIML_URL, to: process.env.TARGET_PHONE_NUMBER, from: process.env.SOURCE_PHONE_NUMBER, /* sendDigits: (true) ? '1' : null  */ })
 			.then((call) => {
 				console.info(`:: Call ${count++} placed with ID [ ${call.sid.substring(0, 8) + '...' + call.sid.substring(call.sid.length - 8)} ]`);
 				count < max + 1 || max === 0 ? placeCall().then(resolve).catch(reject) : resolve();
